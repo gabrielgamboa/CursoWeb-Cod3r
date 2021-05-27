58f245f8f5ccbabc13095820cac7cd9f1d7e4a08
gabrielgamboa <gabriel_gamboa10@hotmail.com>
2021-05-27 13:36:58 -0300

    WebPack Chapter

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,8 +1,15 @@
+// setar variável de ambiente
+const modoDev = process.env.NODE_ENV !== 'production';
+
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
+
     // modo e arquivo de entrada do webpack
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
 
     // qual pasta e nome do arquivo que será gerado
@@ -11,15 +18,51 @@ module.exports = {
         path: __dirname + '/public',
     },
 
+    // configura o webserver do webpack 
+    devServer: {
+        contentBase: "./public",
+        port: 9000,
+    },
+
+    //otimizar os códigos gerados
+    optimization: {
+        minimizer: [
+            //otimiza o arquivo css
+            new OptimizeCSSAssetsPlugin({}),
+        ]
+    },
+
+
+    plugins: [
+        // otimiza o arquivo js
+        new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 6,
+            },
+        }),
+
+        //otimiza o arquivo css
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
+
     module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /\.s?[ac]ss$/,
                 use: [
-                    'style-loader', // Adiciona CSS a dom injetando a tag <style>
-                    'css-loader' // interpreta @import, url()
+                    MiniCssExtractPlugin.loader, // extrai o CSS pra um arquivo externo
+                    //'style-loader', // Adiciona CSS à dom injetando a tag <style>
+                    'css-loader', // interpreta @import, url(),
+                    'sass-loader',
                 ]
-            }
+            }, {
+                // carregar imagens
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            }, 
         ]
     }
 }
